2117b8d7497b39eb691fdd743ed72ceee82196d0
lyshenshing <shing@lyshenshings-MacBook-Pro.local>
2025-09-14 16:25:10 +0700

    Enable create table & insert record code

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"example.com/projectlongterm-api/endpoints/blogs"
+	"example.com/projectlongterm-api/tables"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -100,15 +101,15 @@ func main() {
 	})
 
 	// * Create table
-	// for index, create_table_query := range tables.Query_to_create_table {
-	// 	fmt.Println(index, "Create table");
-	// 	_, err := db.Exec(create_table_query)
-	// 	if err != nil {
-	// 		log.Printf("Error creating table at index %d: %v", index, err)
-	// 	} else {
-	// 		fmt.Printf("Successfully executed table creation query at index %d\n", index)
-	// 	}
-	// }
+	for index, create_table_query := range tables.Query_to_create_table {
+		fmt.Println(index, "Create table")
+		_, err := db.Exec(create_table_query)
+		if err != nil {
+			log.Printf("Error creating table at index %d: %v", index, err)
+		} else {
+			fmt.Printf("Successfully executed table creation query at index %d\n", index)
+		}
+	}
 
 	// * ALter table (add, remove columns)
 	// for index, alter_table_query := range tables.Update_table_string {
@@ -122,7 +123,7 @@ func main() {
 	// }
 
 	// * Insert default article.
-	// tables.Insert_articles(db)
+	tables.Insert_articles(db)
 
 	// port := os.Getenv("PORT");
 	// fmt.Println("PORT is ", port)
